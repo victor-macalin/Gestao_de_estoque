66804bde121369fbe129ce256249f5ec07d6dcd1
Victor Macalin <victormacalin07l@gmail.com>
2026-03-14 23:39:36 -0300

    feat: Cria o ProdutoRequest para receber Requisicoes do usuario

--- src/main/java/com/victor/gestao_de_estoque/controller/request/ProdutoRequest.java
+++ src/main/java/com/victor/gestao_de_estoque/controller/request/ProdutoRequest.java
@@ -1,9 +1,20 @@
 package com.victor.gestao_de_estoque.controller.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.math.BigDecimal;
 
 @Builder
-public record ProdutoRequest(String name, String descricao, Integer quantidade, BigDecimal preco) {
+public record ProdutoRequest(
+        @NotNull
+        String name,
+
+        String descricao,
+
+        BigDecimal preco,
+
+        Long fornecedorId
+
+) {
 }
