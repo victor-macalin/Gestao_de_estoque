e6d307c72b58eb49b3237efa904295da5e29a95e
Victor Macalin <victormacalin07l@gmail.com>
2026-03-16 20:10:22 -0300

    feat: configura Spring Security e endpoint de registro de usuário

--- src/main/java/com/victor/gestao_de_estoque/config/SecurityConfig.java
+++ src/main/java/com/victor/gestao_de_estoque/config/SecurityConfig.java
@@ -1,4 +1,44 @@
 package com.victor.gestao_de_estoque.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+
+        return http
+                .csrf(csfr -> csfr.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                ).build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/victor/gestao_de_estoque/controller/UserController.java
+++ src/main/java/com/victor/gestao_de_estoque/controller/UserController.java
@@ -1,31 +1,40 @@
 package com.victor.gestao_de_estoque.controller;
 
+import com.victor.gestao_de_estoque.controller.request.LoginRequest;
 import com.victor.gestao_de_estoque.controller.request.UserRequest;
 import com.victor.gestao_de_estoque.controller.response.UserResponse;
 import com.victor.gestao_de_estoque.mapper.UserMapper;
 import com.victor.gestao_de_estoque.model.User;
-import com.victor.gestao_de_estoque.repository.UserRepository;
 import com.victor.gestao_de_estoque.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@RestController
-@RequestMapping(name = "/auth")
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
 public class UserController {
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> register (@RequestBody UserRequest userRequest) {
+    public ResponseEntity<UserResponse> register (@RequestBody  UserRequest userRequest) {
         User user = userService.save(UserMapper.toUser(userRequest));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toResponse(user));
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login (@RequestBody LoginRequest loginRequest) {
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password() );
+        Authentication authentication = authenticationManager.authenticate(userAndPass);
 
+        User user = (User) authentication.getPrincipal();
+    }
 }
