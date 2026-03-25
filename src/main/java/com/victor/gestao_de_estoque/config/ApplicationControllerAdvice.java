d976c627477ec17f7170bc1078bfd4644e6f3f91
Victor Macalin <victormacalin07l@gmail.com>
2026-03-25 14:57:04 -0300

    feat(config): adiciona tratamento global de exceções e serviço de token JWT

--- src/main/java/com/victor/gestao_de_estoque/config/ApplicationControllerAdvice.java
+++ src/main/java/com/victor/gestao_de_estoque/config/ApplicationControllerAdvice.java
@@ -1,4 +1,17 @@
 package com.victor.gestao_de_estoque.config;
 
+import com.victor.gestao_de_estoque.exception.UsernameOrPasswordInvalidException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(UsernameOrPasswordInvalidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleNotFoundException (UsernameOrPasswordInvalidException ex) {
+        return ex.getMessage();
+    }
 }
--- src/main/java/com/victor/gestao_de_estoque/config/TokenService.java
+++ src/main/java/com/victor/gestao_de_estoque/config/TokenService.java
@@ -1,4 +1,55 @@
 package com.victor.gestao_de_estoque.config;
 
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.victor.gestao_de_estoque.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@Component
 public class TokenService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return com.auth0.jwt.JWT.create()
+                .withSubject(user.getEmail())
+                .withClaim("userId", user.getId())
+                .withClaim("name", user.getName())
+                .withExpiresAt(Instant.now().plusSeconds(86400))
+                .withIssuedAt(Instant.now())
+                .withIssuer("Api Gestao de estoque")
+                .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> verifyToken (String token) {
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            DecodedJWT jwt = JWT.require(algorithm)
+                    .build()
+                    .verify(token);
+
+
+            return Optional.of(JWTUserData
+                    .builder()
+                    .id(jwt.getClaim("userId").asLong())
+                    .name(jwt.getClaim("name").asString())
+                    .email(jwt.getSubject())
+                    .build());
+        }catch (JWTVerificationException ex ){
+            return  Optional.empty();
+        }
+
+
+    }
 }
