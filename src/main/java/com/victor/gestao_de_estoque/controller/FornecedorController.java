c0558915e3ac0ae4d6f04705385379946f5dc187
Victor Macalin <victormacalin07l@gmail.com>
2026-03-25 14:57:15 -0300

    feat(fornecedor): adiciona endpoints para gerenciamento de fornecedores

--- src/main/java/com/victor/gestao_de_estoque/controller/FornecedorController.java
+++ src/main/java/com/victor/gestao_de_estoque/controller/FornecedorController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/fornecedores")
+@RequestMapping("/api/fornecedores")
 @RequiredArgsConstructor
 public class FornecedorController {
 
