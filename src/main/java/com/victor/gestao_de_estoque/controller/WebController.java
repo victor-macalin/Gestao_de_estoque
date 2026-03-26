b83e38376afbd3fe43eca3de7662a546b88f4489
Victor Macalin <victormacalin07l@gmail.com>
2026-03-26 14:31:19 -0300

    feat: add WebController

---
+++ src/main/java/com/victor/gestao_de_estoque/controller/WebController.java
@@ -0,0 +1,33 @@
+package com.victor.gestao_de_estoque.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping({"/", "/login"})
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+
+    @GetMapping("/produtos")
+    public String produtos() {
+        return "produtos";
+    }
+
+    @GetMapping("/fornecedores")
+    public String fornecedores() {
+        return "fornecedores";
+    }
+
+    @GetMapping("/movimentacoes")
+    public String movimentacoes() {
+        return "movimentacoes";
+    }
+}
