e568672e12e2e20f2b02cdf14415cd2e77f6eb78
Victor Macalin <victormacalin07l@gmail.com>
2026-04-08 19:02:51 -0300

    feat: Adicionando rota register no WebController

--- src/main/java/com/victor/gestao_de_estoque/controller/WebController.java
+++ src/main/java/com/victor/gestao_de_estoque/controller/WebController.java
@@ -16,6 +16,8 @@ public class WebController {
         return "login";
     }
 
+    @GetMapping("/register")
+    public String register() { return "register"; }
 
     @GetMapping("/dashboard")
     public String dashboard() {
